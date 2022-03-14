4281fda0ecde98c04b2a7c589e3a32a56a6f903a
spietrek <spietrek@gmail.com>
2022-03-14 07:43:11 -0400

    cleanup code

--- src/components/organisms/RequireAuthWrapper.tsx
+++ src/components/organisms/RequireAuthWrapper.tsx
@@ -5,11 +5,13 @@ interface RequireAuthProps {
   children?: React.ReactNode
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const RequireAuth = ({ children }: RequireAuthProps): any => {
   const auth = useAuth()
   const location = useLocation()
+  const { user } = auth
 
-  if (auth.user === null) {
+  if (user === null) {
     return <Navigate to="/login" replace state={{ path: location.pathname }} />
   }
 
